cc10546382b517f67dd5db9378912d5a2ab5e0e0
Ray Kman <rkman@nd.edu>
2019-11-05 23:28:47 -0500

    First Commit

---
+++ firstDraft/cornRead.cpp
@@ -0,0 +1,22 @@
+/**********************************************
+* File: sortedlldriver.cpp
+* Author: Ray Kman
+* Email: rkman@nd.edu
+* 
+* This file contains the main driver function
+* that reads the corn.txt file and prints out each
+* symbol and whether or not it is an outright or
+* a spread.
+*
+**********************************************/
+
+#include <fstream>
+#include <iostream>
+std::ifstream infile(corn.txt);
+
+int main() {
+	
+	
+}
+
+
